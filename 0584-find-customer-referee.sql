b9d95f1bb8a41466b33adba07b997efe8999ffac
Chandan <chandan20041220@gmail.com>
2026-09-06 19:23:18 +0530

    solve: Find Customer Referee

---
+++ 0584-find-customer-referee.sql
@@ -0,0 +1,62 @@
+# Write your MySQL query statement below
+Select name
+from Customer
+where referee_id !=2 or referee_id is null;
+
+-- 584. Find Customer Referee
+-- https://leetcode.com/problems/find-customer-referee/
+--
+-- Difficulty : Easy
+-- Topics     : Database
+-- Runtime    : 538 ms
+-- Memory     : 0B
+--
+-- ------------------------------------------------------------
+--
+-- Table: `Customer`
+--
+--     +-------------+---------+
+--     | Column Name | Type    |
+--     +-------------+---------+
+--     | id          | int     |
+--     | name        | varchar |
+--     | referee_id  | int     |
+--     +-------------+---------+
+--     In SQL, id is the primary key column for this table.
+--     Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+--
+-- Find the names of the customer that are either:
+--
+--   1. referred by any customer with `id != 2`.
+--   2. not referred by any customer.
+--
+-- Return the result table in any order.
+--
+-- The result format is in the following example.
+--
+-- Example 1:
+--
+--     Input:
+--     Customer table:
+--     +----+------+------------+
+--     | id | name | referee_id |
+--     +----+------+------------+
+--     | 1  | Will | null       |
+--     | 2  | Jane | null       |
+--     | 3  | Alex | 2          |
+--     | 4  | Bill | null       |
+--     | 5  | Zack | 1          |
+--     | 6  | Mark | 2          |
+--     +----+------+------------+
+--     Output:
+--     +------+
+--     | name |
+--     +------+
+--     | Will |
+--     | Jane |
+--     | Bill |
+--     | Zack |
+--     +------+
+--
+-- Hints:
+--   1. Be careful of the NULL value
